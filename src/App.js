01c57f959f5fa55d0a6357a90d6b9845dc3f61ad
asargabriel <asar.gabriel@gmail.com>
2021-12-04 15:14:02 -0300

    nuevo proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Bienvenidos!</button>
+      <p>APP en Construccion.</p>
+    </div>
+  );
+}
+
+export default App;
